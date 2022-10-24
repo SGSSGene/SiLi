b8a160750d277328b8186fb831f70806f82b125a
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-10-24 20:36:47 +0200

    feat: add printing for fmt

---
+++ src/SiLi/fmt.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "concepts.h"
+
+#include <fmt/format.h>
+
+template <SiLi::_concept::Matrix V>
+struct fmt::formatter<V> {
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(V const& v, FormatContext& ctx) {
+        for (size_t row {0}; row < SiLi::rows_v<V>; ++row) {
+            for (size_t col {0}; col < SiLi::cols_v<V>; ++col) {
+                fmt::format_to(ctx.out(), "{} ", v(row, col));
+            }
+            fmt::format_to(ctx.out(), "\n");
+        }
+        return ctx.out();
+    }
+};
--- src/exampleSiLi/example.cpp
+++ src/exampleSiLi/example.cpp
@@ -1,5 +1,6 @@
 #include <SiLi/SiLi.h>
 #include <SiLi/ostream.h>
+#include <SiLi/fmt.h>
 
 int main() {
     // m1 is of type Matrix<3, 2, double>
@@ -18,14 +19,14 @@ int main() {
     auto m4 = m2 * m1;
 
     // print matrix
-    std::cout << "m1:\n" << m1 << "\n\n";
-    std::cout << "m2:\n" << m2 << "\n\n";
-    std::cout << "m3:\n" << m3 << "\n\n";
-    std::cout << "m4:\n" << m4 << "\n\n";
+    fmt::print("m1:\n{}\n\n", m1);
+    fmt::print("m2:\n{}\n\n", m2);
+    fmt::print("m3:\n{}\n\n", m3);
+    fmt::print("m4:\n{}\n\n", m4);
 
     // print matrix element by element
-    for (int row = 0; row < rows(m1); ++row) {
-        for (int col = 0; col < cols(m1); ++col) {
+    for (size_t row = 0; row < rows(m1); ++row) {
+        for (size_t col = 0; col < cols(m1); ++col) {
             std::cout << m1(row, col) << "\n";
         }
     }
